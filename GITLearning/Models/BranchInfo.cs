35a3e65d4f87be2180473f31f0acd170bc3d587d
Prithvi Pal Singh <Prithvi.Coer@outlook.com>
2017-06-17 20:01:50 +0800

    Add project files.

---
+++ GITLearning/Models/BranchInfo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GITLearning.Models
+{
+    class BranchInfo
+    {
+        public string Name { get; set; }
+        public int CommitCount { get; set; }
+    }
+}
